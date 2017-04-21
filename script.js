552caf32e67df6a4cf4a3d24ff457e38c317534e
RAN-PC\Ran Kuang <rkuang@id.iit.edu>
2017-04-21 10:37:32 -0500

    take the SWITCH out

--- script.js
+++ script.js
@@ -5,28 +5,13 @@ var div = [];
 for (var i = 0; i < 5; i++) {
     div[i] = document.createElement("div");
     div[i].className = element[i];
-    switch(i) {
-        case 0:
-            document.body.appendChild(div[i]);
-            break;
-        case 1:
-            div[0].appendChild(div[i]);
-            break;
-        case 2:
-            div[0].appendChild(div[i]);
-            break;
-        case 3:
-            div[0].appendChild(div[i]);
-            break;
-        // why can't I merge the three same ones using
-        // case 1 && 2 && 3:
-        // div[0].appendChild(div[i]);
-        // break;           
-        case 4:
-            div[3].appendChild(div[i]);
-            break;
-    }
 };
+
+    document.body.appendChild(div[0]);
+    div[0].appendChild(div[1]);
+    div[0].appendChild(div[2]);
+    div[0].appendChild(div[3]);
+    div[3].appendChild(div[4]);
 
 var x = 1;
 while (x <= 5) {
